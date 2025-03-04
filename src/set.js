51b11eb80bc429e9e67a4abc823985824bd3e618
Bairui Su <subairui@icloud.com>
2025-03-04 16:34:17 -0500

    Tidy APIs (#10)

--- src/set.js
+++ src/set.js
@@ -10,13 +10,3 @@ export function vec$set(a, b) {
   this.y = b;
   return this;
 }
-
-export function vec$setX(x) {
-  this.x = x;
-  return this;
-}
-
-export function vec$setY(y) {
-  this.y = y;
-  return this;
-}
